9936a7fe78df3883a8d09a55a95b990075ec24c8
Snikkertjuh <xeonnode@gmail.com>
2022-10-24 00:57:13 +0200

    Got event system working

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import microcord
 
 client = microcord.Client(open(".token", "r").read())
 
-@client.event
-async def ready():
-    print("Ready!")
+@client.event("READY")
+def test_function():
+    print(f"[ INFO ] Logged in as {client.user.username}#{client.user.discriminator}")
 
 client.run()
--- microcord/client.py
+++ microcord/client.py
@@ -4,8 +4,11 @@ import json
 import time
 
 from types import SimpleNamespace
+from functools import wraps
 
-class GatewayMessage():
+from .user import User
+
+class GatewayMessage:
     def __init__(self, data: str):
         try:
             data = json.loads(data)
@@ -26,45 +29,42 @@ class GatewayMessage():
 class Client:
     def __init__(self, token):
         self.token = token
-        self.connected = False
 
         self.ready = None
         self.guild_create = None
         self.message_create = None
 
     def event(self, event: str = None):
-        threading.Thread(target=self.event_handler, args=(event,)).start()
+        def decorator(function):
+            if event == "READY": self.ready = function
+            def wrapper(*args, **kwargs):
+                return function(*args, **kwargs)
+            return wrapper
+        return decorator
 
     def event_handler(self, event: str = None):
-        while not self.connected:
-            pass
-
         while True:
             try:
                 msg = GatewayMessage(self.ws.recv())
                 if msg.op == 11:
-                    print("Heartbeat acknowledged!")
+                    pass
                 else: 
                     if msg.event == "READY" and self.ready:
+                        self.user = User(self.token, SimpleNamespace(**msg.data.user))
                         self.ready()
             except websocket._exceptions.WebSocketConnectionClosedException:
                 print("Socket closed")
-                input()
+                exit()
 
     def connection_handler(self):
-        print("Starting connection handler...")
         while self.interval:
             self.ws.send(json.dumps({
                 "op": 1,
                 "d": None
             }))
-            print("Heartbeat sent!")
             time.sleep(self.interval)
-        else:
-            print("Heartbeat interval is 0, stopping heartbeat.")
 
     def authentication_handler(self):
-        print("Authenticating with Discord...")
         self.ws.send(json.dumps({
             "op": 2,
             "d": {
@@ -77,11 +77,10 @@ class Client:
                 },
             }
         }))
-        print("Authenticated with Discord!")
 
     def run(self):
         self.ws = websocket.create_connection("wss://gateway.discord.gg/?v=6&encoding=json")
         self.interval = GatewayMessage(self.ws.recv()).data.heartbeat_interval / 1000
-        self.connected = True
         threading.Thread(target=self.authentication_handler).start()
         threading.Thread(target=self.connection_handler).start()
+        threading.Thread(target=self.event_handler).start()
