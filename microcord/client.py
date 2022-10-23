50214f686afc89e9a9c8a14022dacbe3fcc1da15
Snikkertjuh <xeonnode@gmail.com>
2022-10-23 06:24:05 +0200

    Gateway connection working

--- microcord/client.py
+++ microcord/client.py
@@ -7,32 +7,41 @@ from types import SimpleNamespace
 
 class GatewayMessage():
     def __init__(self, data: str):
-        data = json.loads(data)
-        self.op = data["op"]
-        self.data = SimpleNamespace(**data["d"])
-        if "name" in data:
-            self.name = data["name"]
-        else:
-            self.name = None
+        try:
+            data = json.loads(data)
+            self.op = data["op"]
+            if data["d"]:
+                self.data = SimpleNamespace(**data["d"])
+            else:
+                self.data = None
+            if "t" in data:
+                self.event = data["t"]
+            else:
+                self.event = None
+        except:
+            self.op = None
+            self.data = None
+            self.event = None
 
 class Client:
     def __init__(self, token):
+        self.loop = asyncio.get_event_loop()
         self.token = token
-        self.ws = websocket.create_connection("wss://gateway.discord.gg/?v=6&encoding=json")
-        self.interval = GatewayMessage(self.ws.recv()).data.heartbeat_interval / 1000
-        loop = asyncio.get_event_loop()
-        loop.create_task(self.authentication_handler())
 
     def event(self, event: str = None):
-        loop = asyncio.get_event_loop()
-        loop.create_task(self.event_handler(event))
+        self.loop.create_task(self.event_handler(event))
 
-    async def event_handler(self, event: str):
+    async def event_handler(self, event: str = None):
         while True:
-            data = GatewayMessage(self.ws.recv())
-            print(data)
+            msg = GatewayMessage(self.ws.recv())
+            if msg.op == 11:
+                print("Heartbeat acknowledged!")
+            else: 
+                print(msg.event)
+            await asyncio.sleep(0.1)
 
     async def connection_handler(self):
+        print("Starting connection handler...")
         while self.interval:
             self.ws.send(json.dumps({
                 "op": 1,
@@ -40,8 +49,10 @@ class Client:
             }))
             print("Heartbeat sent!")
             await asyncio.sleep(self.interval)
+        else:
+            print("Heartbeat interval is 0, stopping heartbeat.")
 
-    async def authentication_handler(self):
+    def authentication_handler(self):
         print("Authenticating with Discord...")
         self.ws.send(json.dumps({
             "op": 2,
@@ -57,5 +68,7 @@ class Client:
         print("Authenticated with Discord!")
 
     def run(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.connection_handler())
+        self.ws = websocket.create_connection("wss://gateway.discord.gg/?v=6&encoding=json")
+        self.interval = GatewayMessage(self.ws.recv()).data.heartbeat_interval / 1000
+        self.authentication_handler()
+        self.loop.run_until_complete(self.connection_handler())
